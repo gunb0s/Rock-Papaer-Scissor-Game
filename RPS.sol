da82291992ffb6aef62cce6dce016651305229da
gunbos1031 <69377496+gunbos1031@users.noreply.github.com>
2022-03-21 15:36:25 +0900

    Implement verying signature about rock, paper, scissor

--- RPS.sol
+++ RPS.sol
@@ -117,4 +117,52 @@ contract RPS {
 
         rooms[roomNum].gameStatus = GameStatus.STATUS_COMPLETE;
     }
+
+    function verify(address _sender, bytes memory _sig) public pure returns (int) {
+        bytes32 message_rock = ethMessageHash("rock");
+        bytes32 message_paper = ethMessageHash("paper");
+        bytes32 message_scissor = ethMessageHash("scissor");
+        
+        if (recover(message_rock, _sig) == _sender) {
+            return 0;
+        } else if (recover(message_paper, _sig) == _sender) {
+            return 1;
+        } else if (recover(message_scissor, _sig) == _sender) {
+            return 2;
+        } else {
+            return -1;
+        }
+    }
+
+    function recover(bytes32 _hash, bytes memory _sig) internal pure returns (address) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        if (_sig.length != 65) {
+            return address(0);
+        }
+
+        assembly {
+            r := mload(add(_sig, 32))
+            s := mload(add(_sig, 64))
+            v := byte(0, mload(add(_sig, 96)))
+        }
+        if (v < 27) {
+            v += 27;
+        }
+
+        if (v != 27 && v != 28) {
+            return address(0);
+        } else {
+            return ecrecover(_hash, v, r, s);
+        }
+    }
+
+    function ethMessageHash(string memory _message) internal pure returns (bytes32) {
+         return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", _message)
+            );
+    }
 }
