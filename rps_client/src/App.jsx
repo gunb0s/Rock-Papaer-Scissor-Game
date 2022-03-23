d8b4e1c02a40626f6be43e53ca63ca2297d6d518
gunbos1031 <gunbos1031@gmail.com>
2022-03-23 23:56:19 +0900

    Implement game lists with styled-component and bootstrap

--- rps_client/src/App.jsx
+++ rps_client/src/App.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./components/Navigation";
+import GameRooms from "./components/GameRooms";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  padding-left: 5rem;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+`;
 
 function App() {
   return (
-    <div>
+    <Container>
       <Navigation />
-    </div>
+      <Main>
+        <GameRooms />
+      </Main>
+    </Container>
   );
 }
 
---
+++ rps_client/src/components/GameRooms.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+
+const GameRooms = () => {
+  return (
+    <ListGroup variant="flush">
+      <ListGroup.Item>
+        Cras justo odiosssssssssssssssssssssssssssssssssssssss
+      </ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+    </ListGroup>
+  );
+};
+
+export default GameRooms;
